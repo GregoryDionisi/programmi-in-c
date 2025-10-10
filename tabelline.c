44bf4b45462f482217ffb7911d3aede48b4c315a
GregoryDionisi <gregoryd324@gmail.com>
2025-10-10 11:53:46 +0200

    aggiunta tabelline

--- tabelline.c
+++ tabelline.c
@@ -6,9 +6,9 @@ int main(){
         printf("Inserisci un numero per visualizzare la tabellina (0 per uscire):\n");
         scanf("%d", &numero);
         if (numero != 0){
-        for (i = 0; i < 10; i++){
-            prodotto = numero * (i+1);
-            printf("%d * %d = %d\n", numero, i+1, prodotto);
+        for (i = 1; i < 11; i++){
+            prodotto = numero * i;
+            printf("%d * %d = %d\n", numero, i, prodotto);
         }
     }
     } while (numero != 0);

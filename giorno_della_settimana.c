a835cdfa1c74d6d63e46f594962ad2e4b18b267a
GregoryDionisi <gregoryd324@gmail.com>
2025-10-10 10:30:00 +0200

    aggiunta giorno_della_settimana

--- giorno_della_settimana.c
+++ giorno_della_settimana.c
@@ -4,35 +4,35 @@ int main(){
     int numero;
     do
     {
-    printf("\nInserisci un numero da 1 a 7 (0 per uscire):\n");
+    printf("Inserisci un numero da 1 a 7 (0 per uscire):\n");
     scanf("%d", &numero);
     if(numero == 1)
     {
-        printf("Lunedì");
+        printf("Lunedì\n");
     }
     else if (numero == 2)
     {
-        printf("Martedì");
+        printf("Martedì\n");
     } 
     else if (numero == 3)
     {
-        printf("Mercoledì");
+        printf("Mercoledì\n");
     }
     else if (numero == 4)
     {
-        printf("Giovedì");
+        printf("Giovedì\n");
     }
     else if (numero == 5)
     {
-        printf("Venerdì");
+        printf("Venerdì\n");
     }
     else if (numero == 6)
     {
-        printf("Sabato");
+        printf("Sabato\n");
     }
     else if (numero == 7)
     {
-        printf("Domenica");
+        printf("Domenica\n");
     }
     } while(numero != 0);
     return 0;

cbbd58c4e9ae6592bd49078c4ed50b1ea5699fe6
GregoryDionisi <gregoryd324@gmail.com>
2025-10-10 10:20:22 +0200

    aggiunta giorno_della_settimana

---
+++ giorno_della_settimana.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main(){
+    int numero;
+    while(numero != 0){
+    printf("\nInserisci un numero da 1 a 7 (0 per uscire):\n");
+    scanf("%d", &numero);
+    if(numero == 1)
+    {
+        printf("Lunedì");
+    }
+    else if (numero == 2)
+    {
+        printf("Martedì");
+    } 
+    else if (numero == 3)
+    {
+        printf("Mercoledì");
+    }
+    else if (numero == 4)
+    {
+        printf("Giovedì");
+    }
+    else if (numero == 5)
+    {
+        printf("Venerdì");
+    }
+    else if (numero == 6)
+    {
+        printf("Sabato");
+    }
+    else if (numero == 7)
+    {
+        printf("Domenica");
+    }
+    }
+    return 0;
+}

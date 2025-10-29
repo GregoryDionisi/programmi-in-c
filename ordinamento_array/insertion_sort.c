a0b4c2e35aca1ade0047b0dfcc70b872846dc36e
GregoryDionisi <gregoryd324@gmail.com>
2025-10-29 11:19:41 +0100

    Aggiunta insertion sort

--- ordinamento_array/insertion_sort.c
+++ ordinamento_array/insertion_sort.c
@@ -10,15 +10,16 @@ int main(){
         printf("%d \t", v[i]); // \t è il tabulatore orizzontale, mette i valori in colonne
     }
 
-    // Ordinamento dell'array con Bubble Sort
-    for(i=0; i<9; i++){ // ripeto il ciclo più volte per ordinare tutti gli elementi. Usiamo 9 perchè il numero più grande, che si trova in fondo, ormai non lo contiamo più
-        for(j=0; j<9-i; j++){ // confronto gli elementi adiacenti
-            if(v[j]>v[j+1]){
-                temp = v[j]; // scambio i due elementi
-                v[j] = v[j+1];
-                v[j+1] = temp;
-            }
-        }        
+    // Ordinamento dell'array con l'Insertion Sort
+    for(j = 1; j < 10; j++){
+        temp = v[j];
+        i = j - 1;
+
+        while(i >= 0 && v[i] > temp){
+            v[i+1] = v[i];
+            i--;
+        }
+        v[i+1] = temp;
     }
 
     printf("\nStampo array ordinato\n");
